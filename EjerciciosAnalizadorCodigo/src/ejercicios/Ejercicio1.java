0c1f3e338d09575d6920e02c016bb4da57a76692
aless <aless@HPaleleotta>
2022-12-01 12:23:58 +0100

    .

---
+++ EjerciciosAnalizadorCodigo/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,36 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int intentos = 4;
+		int numeroIntroducido;
+		boolean acertado = false;
+
+		do {
+			System.out.print("Introduzca la clave de la caja fuerte: ");
+			numeroIntroducido = s.nextInt();
+
+			if (numeroIntroducido == 8888) {
+				acertado = true;
+			} else {
+				System.out.println("Clave incorrecta");
+			}
+
+			intentos--;
+
+		} while ((intentos > 0) && (!acertado));
+
+		if (acertado) {
+			System.out.println("Ha abierto la caja fuerte.");
+		} else {
+			System.out.println("Lo siento, ha agotado las 4 oportunidades.");
+		}
+		s.close();
+	}
+}
